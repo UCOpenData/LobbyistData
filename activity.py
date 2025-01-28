c0f27f81b3bdae6f35495d635a2b3177cac94fcb
bdarmstrong <bendyarmstrong@gmail.com>
2025-01-28 17:23:26 -0600

    Testing streamlit pie chart

--- activity.py
+++ activity.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+df = pd.read_csv('datasets/Lobbyist_Data_-_Lobbying_Activity_20250121.csv')
+
+plt.style.use('_mpl-gallery-nogrid')
+
+# make data
+x = [1, 2, 3, 4]
+colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(x)))
+
+# plot
+fig, ax = plt.subplots()
+ax.pie(x, colors=colors, radius=3, center=(4, 4),
+       wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
+
+st.pyplot(plt)
