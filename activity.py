517b0aa23e06449e2c86ffecf778f0b5088b7cad
bdarmstrong <bendyarmstrong@gmail.com>
2025-02-04 16:49:20 -0600

    Changed csv names

--- activity.py
+++ activity.py
@@ -1,24 +1,17 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import streamlit as st
+import pandas as pd
 
-df = pd.read_csv('datasets/Lobbyist_Data_-_Lobbying_Activity_20250121.csv')
+@st.cache_data
+def load_data():
+    return pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
 
-plt.style.use('_mpl-gallery-nogrid')
+def main():
+    data = load_data()
+#     st.sidebar.header("Filters")
+#     location_counts = data.groupby(["STATE"]).size().reset_index(name="COUNT")
+#     top_locations = location_counts.sort_values(by="COUNT", ascending=False).head(10)
+#     st.title("Top 10 Most Common Locations in Lobbyist Data")
+#     st.bar_chart(top_locations.set_index("STATE")["COUNT"])
 
-# make data
-x = [1, 2, 3, 4]
-colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(x)))
-
-# plot
-fig, ax = plt.subplots()
-ax.pie(x, colors=colors, radius=3, center=(4, 4),
-       wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
-
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
-
-plt.show()
-
-st.pyplot(plt)
+if __name__  == '__main__':
+    main()
--- lobbyist_people.py
+++ lobbyist_people.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @st.cache_data
 def load_data():
-    return pd.read_csv('./datasets/Lobbyist_Data_-_Lobbyists_20250121.csv')
+    return pd.read_csv('./datasets/LD_Lobbyists.csv')
 
 def main():
     data = load_data()
