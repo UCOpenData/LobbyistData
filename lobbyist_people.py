68d5d8a656943c5154a914443c9962f9bac001d2
Ahsokastar <catherine.todd@gmail.com>
2025-02-04 17:58:42 -0600

    w

--- lobbyist_people.py
+++ lobbyist_people.py
@@ -8,29 +8,35 @@ def load_data():
     return pd.read_csv('./datasets/LD_Lobbyists.csv')
 
 def main():
+    st.sidebar.title("Navigation")
     data = load_data()
-    st.sidebar.header("Filters")
-    state_counts = data["STATE"].value_counts().reset_index()
-    state_counts.columns = ["State", "Count"]
-    top_states = state_counts.head(10)
-    sns.set_style("whitegrid")
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(y=top_states["State"], x=top_states["Count"], palette="Blues_r", ax=ax)
-    ax.set_xlabel("Count")
-    ax.set_ylabel("State")
-    ax.set_title("Top 10 Most Common States in Lobbyist Data")
-    st.title("Top 10 Most Common States in Lobbyist Data")
-    st.pyplot(fig)
-    company_counts = data["EMPLOYER_NAME"].value_counts().reset_index()
-    company_counts.columns = ["Employer", "Count"]
-    top_companies = company_counts.head(20)
-    fig_1, ax_1 = plt.subplots(figsize=(5, 6))
-    sns.barplot(y=top_companies["Employer"], x=top_companies["Count"], palette="Blues_r", ax=ax_1)
-    ax_1.set_xlabel("Count")
-    ax_1.set_ylabel("Employer")
-    ax_1.set_title("Top 10 Most Common Employers in Lobbyist Data")
-    st.title("Top 10 Most Common Employers in Lobbyist Data")
-    st.pyplot(fig_1)
+    page = st.sidebar.radio("Go to", ["Lobbyist Search", "Top 10 States"])
+    if page == 'Top 10 States':
+        
+        
+        state_counts = data["STATE"].value_counts().reset_index()
+        state_counts.columns = ["State", "Count"]
+        top_states = state_counts.head(10)
+        sns.set_style("whitegrid")
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.barplot(y=top_states["State"], x=top_states["Count"], palette="Blues_r", ax=ax)
+        ax.set_xlabel("Count")
+        ax.set_ylabel("State")
+        ax.set_title("Top 10 Most Common States in Lobbyist Data")
+        st.title("Top 10 Most Common States in Lobbyist Data")
+        st.pyplot(fig)
+        company_counts = data["EMPLOYER_NAME"].value_counts().reset_index()
+        company_counts.columns = ["Employer", "Count"]
+        top_companies = company_counts.head(20)
+        fig_1, ax_1 = plt.subplots(figsize=(5, 6))
+        sns.barplot(y=top_companies["Employer"], x=top_companies["Count"], palette="Blues_r", ax=ax_1)
+        ax_1.set_xlabel("Count")
+        ax_1.set_ylabel("Employer")
+        ax_1.set_title("Top 10 Most Common Employers in Lobbyist Data")
+        st.title("Top 10 Most Common Employers in Lobbyist Data")
+        st.pyplot(fig_1)
+    else:
+        search_query = st.text_input("Enter a lobbyist's name:", "")
 
 
 if __name__  == '__main__':
