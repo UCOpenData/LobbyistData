f1a90d3cd7bd75bd942f5d5f23994233b8b1417f
Ahsokastar <catherine.todd@gmail.com>
2025-02-04 16:23:23 -0600

    descending

--- lobbyist_people.py
+++ lobbyist_people.py
@@ -9,7 +9,7 @@ def main():
     data = load_data()
     st.sidebar.header("Filters")
     location_counts = data.groupby(["CITY", "STATE"]).size().reset_index(name="COUNT")
-    top_locations = location_counts.sort_values(by="COUNT", ascending=True).head(10)
+    top_locations = location_counts.sort_values(by="COUNT", ascending=False).head(10)
     st.title("Top 10 Most Common Locations in Lobbyist Data")
     st.bar_chart(top_locations.set_index("CITY")["COUNT"])
 
