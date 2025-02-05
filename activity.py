9d3bc8a3494eb1d5744871f993bb4583081c0ee0
bdarmstrong <bendyarmstrong@gmail.com>
2025-02-04 18:03:50 -0600

    Created lobbying activity bar graph

--- activity.py
+++ activity.py
@@ -1,17 +1,28 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 @st.cache_data
 def load_data():
     return pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
 
+def activity_by_department(data):
+     department_counts = data['DEPARTMENT'].value_counts()
+     top_departments = department_counts[:10]
+     sns.set_style("whitegrid")
+     fig, ax = plt.subplots(figsize=(10, 6))
+     sns.barplot(y=top_departments.keys(), x=top_departments.values, palette="Blues_r", ax=ax)
+     ax.set_xlabel("Count")
+     ax.set_ylabel("Department")
+     st.title("Top 10 departments with the most lobbying activity")
+     st.pyplot(fig)
+
 def main():
-    data = load_data()
-#     st.sidebar.header("Filters")
-#     location_counts = data.groupby(["STATE"]).size().reset_index(name="COUNT")
-#     top_locations = location_counts.sort_values(by="COUNT", ascending=False).head(10)
-#     st.title("Top 10 Most Common Locations in Lobbyist Data")
-#     st.bar_chart(top_locations.set_index("STATE")["COUNT"])
+     data = load_data()
+#      activity_by_department(data)
+     print(data['CREATED_DATE'])
+
 
 if __name__  == '__main__':
     main()
