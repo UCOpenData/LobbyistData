20efcda1cfbffadaf878d7861d636fc5b5afb664
bdarmstrong <bendyarmstrong@gmail.com>
2025-02-25 17:57:34 -0600

    Created Individual_Data page with table view and departments graph

--- activity.py
+++ activity.py
@@ -2,26 +2,37 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 @st.cache_data
 def load_data():
     return pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
 
-def activity_by_department(data):
-     department_counts = data['DEPARTMENT'].value_counts()
-     top_departments = department_counts[:10]
-     sns.set_style("whitegrid")
-     fig, ax = plt.subplots(figsize=(10, 6))
-     sns.barplot(y=top_departments.keys(), x=top_departments.values, palette="Blues_r", ax=ax)
-     ax.set_xlabel("Count")
-     ax.set_ylabel("Department")
-     st.title("Top 10 departments with the most lobbying activity")
-     st.pyplot(fig)
+def activity_by_department(activity):
+    department_counts = activity['DEPARTMENT'].value_counts()
+    top_departments = department_counts[:10]
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(y=top_departments.keys(), x=top_departments.values, palette="Blues_r", ax=ax)
+    ax.set_xlabel("Count")
+    ax.set_ylabel("Department")
+    st.title("Top 10 departments with the most lobbying activity")
+    st.pyplot(fig)
+
+def year_slider():
+    date = st.slider("Select date",
+                     min_value = datetime(2014,1,1),
+                     max_value = datetime(2024,12,1),
+                     value = datetime(2014,1,1),
+                     step = timedelta(days = 1))
+    return date
 
 def main():
-     data = load_data()
-#      activity_by_department(data)
-     print(data['CREATED_DATE'])
+    activity = load_data()
+    activity['DATE'] = pd.to_datetime(activity['PERIOD_START'])
+    date = year_slider()
+    activity_upto_date = activity[(activity['DATE'] < date) | (activity['DATE'] == date)]
+    activity_by_department(activity_upto_date)
 
 
 if __name__  == '__main__':
--- pages/Individual_Data.py
+++ pages/Individual_Data.py
@@ -2,8 +2,15 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from activity import activity_by_department
 
 params = st.query_params["lobbyist_id"]
-data = pd.read_csv
+data = pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
 
-st.write(params)
+person_id = int(params)
+
+activity_subset = data[data["LOBBYIST_ID"] == person_id]
+
+st.dataframe(activity_subset)
+
+activity_by_department(activity_subset)
