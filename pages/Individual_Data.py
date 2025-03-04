c7ff894b1a6f8acd0462e76faddc70fbe15f6652
bdarmstrong <bendyarmstrong@gmail.com>
2025-03-04 17:43:16 -0600

    Added "no activity found" text

--- pages/Individual_Data.py
+++ pages/Individual_Data.py
@@ -11,11 +11,14 @@ person_id = int(params)
 
 activity_subset = activity[activity["LOBBYIST_ID"] == person_id]
 
-first_name = activity_subset["LOBBYIST_FIRST_NAME"].iloc[0]
-last_name = activity_subset["LOBBYIST_LAST_NAME"].iloc[0]
+if (len(activity_subset) > 0):
+    first_name = activity_subset["LOBBYIST_FIRST_NAME"].iloc[0]
+    last_name = activity_subset["LOBBYIST_LAST_NAME"].iloc[0]
 
-st.write(first_name + " " + last_name)
+    st.write(first_name + " " + last_name)
 
-st.dataframe(activity_subset)
+    st.dataframe(activity_subset)
 
-activity_by_department(activity_subset)
+    activity_by_department(activity_subset)
+else:
+    st.write("No lobbying activity found for this lobbyist")
