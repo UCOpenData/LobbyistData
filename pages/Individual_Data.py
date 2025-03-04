d4619f8182175133456695ecf414747b50661b06
bdarmstrong <bendyarmstrong@gmail.com>
2025-03-04 17:19:38 -0600

    Fixed bug with lobbyist display

--- pages/Individual_Data.py
+++ pages/Individual_Data.py
@@ -5,11 +5,16 @@ import matplotlib.pyplot as plt
 from activity import activity_by_department
 
 params = st.query_params["lobbyist_id"]
-data = pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
+activity = pd.read_csv('./datasets/LD_Lobbying_Activity.csv')
 
 person_id = int(params)
 
-activity_subset = data[data["LOBBYIST_ID"] == person_id]
+activity_subset = activity[activity["LOBBYIST_ID"] == person_id]
+
+first_name = activity_subset["LOBBYIST_FIRST_NAME"].iloc[0]
+last_name = activity_subset["LOBBYIST_LAST_NAME"].iloc[0]
+
+st.write(first_name + " " + last_name)
 
 st.dataframe(activity_subset)
 
