afb8dddfac229500ca342c9cf668d3330e14bd6c
Ahsokastar <catherine.todd@gmail.com>
2025-04-23 17:35:07 -0500

    h

---
+++ pages/video.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from sidebar import init_sidebar
+init_sidebar()
+
+st.video('per_dep2.mp4')
+st.write("BACP stands for Business Affairs and Consumer Protection")
