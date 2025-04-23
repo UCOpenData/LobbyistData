41fac91dd56e66e43536e24989a497a6d8b9791d
Ahsokastar <catherine.todd@gmail.com>
2025-04-23 17:38:42 -0500

    h

--- sidebar.py
+++ sidebar.py
@@ -4,3 +4,4 @@ def init_sidebar():
     st.sidebar.page_link('pages/Lobbyist_Search.py', label='Lobbyist Search')
     st.sidebar.page_link('pages/Department_Search.py', label='Department Search')
     st.sidebar.page_link('pages/Top_10_Charts.py', label='Top 10 Charts')
+    st.sidebar.page_link('pages/video.py', label='Video of progression')
